2c96589d4412f04c5938728b769807487d46e00c
Andrea_Sanna <47662221+AthlonsDev@users.noreply.github.com>
2024-04-18 11:56:02 +0000

    fixes in cors

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const weather_routes_js_1 = __importDefault(require("./routes/weather_routes.js"
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = 3000;
-app.use("/api/weather", weather_routes_js_1.default);
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use("/api/weather", weather_routes_js_1.default);
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import weatherRoute from "./routes/weather_routes.js";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
-app.use("/api/weather", weatherRoute);
 app.use(express.json());
 app.use(cors());
+app.use("/api/weather", weatherRoute);
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 });
